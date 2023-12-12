1aa8d37fd3b16145126f09ecf18258df0a129549
shun2218-dev <kohaku1015game@gmail.com>
2023-12-13 01:28:16 +0900

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
-import { Anchor } from "@mantine/core"
-import "./App.css";
+import { Anchor } from '@mantine/core';
+import './App.css';
 import '@mantine/core/styles.css';
 
 function App() {
-  // const [greetMsg, setGreetMsg] = useState("");
-  // const [name, setName] = useState("");
-
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
-
   return (
     <div className="container">
-      <Anchor href="/upload">
-        Upload
-      </Anchor>
+      <Anchor href="/upload">Upload</Anchor>
     </div>
   );
 }
